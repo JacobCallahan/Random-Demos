a1028282a0dd5f292f2bbd61fccf0505599be6bd
JacobCallahan <jacob.callahan05@gmail.com>
2014-08-07 21:05:50 -0400

    Adding WIP AVL tool demo
    Majority of logic and input is handled. Need to work on drawing the
    nodes as part of the tree structure.

---
+++ AVL Tool/AVL Tool/AVLNode.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AVL_Tool {
+    class AVLNode {
+        int value;
+        AVLNode parentNode;
+        AVLNode leftChild;
+        AVLNode rightChild;
+        System.Windows.Forms.Label myLabel;
+
+        public AVLNode(int nodeValue) {
+            //the simple constructor will take in an integer value
+            //and sets ourself as the parent node
+            this.value = nodeValue;
+            this.parentNode = this;
+            this.leftChild = null;
+            this.rightChild = null;
+            this.myLabel = null;
+        }
+
+        public AVLNode(int nodeValue, AVLNode ParentNode) {
+            //the preferred constructor will take in an integer value
+            //and also take in a ParentNode
+            this.value = nodeValue;
+            this.parentNode = ParentNode;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+        //start our get set functions
+        public int Value {
+            get { return this.value; }
+            set { this.value = Value; }
+        }
+
+        public AVLNode ParentNode {
+            get { return this.parentNode; }
+            set { this.parentNode = ParentNode; }
+        }
+
+        public AVLNode LeftChild {
+            get { return this.leftChild; }
+            set { this.leftChild = LeftChild; }
+        }
+
+        public AVLNode RightChild {
+            get { return this.rightChild; }
+            set { this.rightChild = RightChild; }
+        }
+
+        public void addChild(AVLNode newChild) {
+            //This function will put a new child node into the tree
+            if (newChild.value <= this.value) {
+                //check to see of we have an empty slot in the left child position
+                if (this.leftChild == null) {
+                    //if so, fill it
+                    newChild.parentNode = this;
+                    this.leftChild = newChild;
+                } else {
+                    //if not, then pass the new node on to it
+                    this.leftChild.addChild(newChild);
+                }
+            } else {
+                if (this.rightChild == null) {
+                    newChild.parentNode = this;
+                    this.rightChild = newChild;
+                } else {
+                    this.rightChild.addChild(newChild);
+                }
+            }
+        }
+
+        public List<int> printTree() {
+            //this function will return a sorted list of all the values
+            //below and including this node
+            List<int> tempList = new List<int>();
+            if (this.leftChild != null) {
+                //if we have a left child add its list to our list first
+                tempList.AddRange(this.leftChild.printTree());
+            }
+            //add our value no matter what
+            tempList.Add(this.value);
+            if (this.rightChild != null) {
+                //if we have a right child add its list to our list last
+                tempList.AddRange(this.rightChild.printTree());
+            }
+            return tempList;
+        }
+
+        public String printTreeString() {
+            //this function will return a sorted list of all the values
+            //below and including this node
+            String tempString = "";
+            if (this.leftChild != null) {
+                //if we have a left child add its list to our list first
+                tempString = string.Concat(this.leftChild.printTreeString(),", ");
+            }
+            //add our value no matter what
+            tempString = string.Concat(tempString, Convert.ToString(this.value));
+            if (this.rightChild != null) {
+                //if we have a right child add its list to our list last
+                tempString = string.Concat(tempString, ", ", this.rightChild.printTreeString());
+            }
+            return tempString;
+        }
+
+        public int getDepth() {
+            int thisDepth = 0;
+            if (this.leftChild != null) {
+                //if we have a left child set its depth as the current value
+                thisDepth = this.leftChild.getDepth();
+            }
+            if (this.rightChild != null) {
+                //if we have a right child, see if its depth is greater than our left
+                int tempDepth = this.rightChild.getDepth();
+                //if so, set this depth equal to our right child's depth
+                if (tempDepth > thisDepth) { thisDepth = tempDepth; }
+            }
+            return thisDepth + 1; //finally add 1 to account for ourselves.
+        }
+
+        public void swap(AVLNode replacementNode) {
+            //this function is the brunt of the balancing movement
+            //this function should only be called on a parent node
+            //to swap with one of its children 
+            if (this.parentNode != this) { 
+                replacementNode.ParentNode = this.parentNode;
+            } else {
+                //if we are at the top, make the other node its own parent
+                replacementNode.ParentNode = replacementNode; 
+            }
+
+            if (this.leftChild == replacementNode) {
+                //swap the position of this node with the supplanting node
+                if (this.parentNode != this) {
+                    if (replacementNode.Value > this.parentNode.Value) {
+                        this.parentNode.RightChild = replacementNode;
+                    } else {
+                        this.parentNode.LeftChild = replacementNode;              
+                    }
+                }
+                this.leftChild = replacementNode.RightChild;           //adopt its appropriate child
+                replacementNode.RightChild = replacementNode;
+                if (this.leftChild != null) {this.leftChild.ParentNode = this;} //notify the child of its new parent
+
+            } else if (this.rightChild == replacementNode) {
+                if (this.parentNode != this) {
+                    if (replacementNode.Value < this.parentNode.Value) {
+                        this.parentNode.LeftChild = replacementNode;
+                } else {
+                    this.parentNode.RightChild = replacementNode;              
+                }
+                }
+                this.rightChild = replacementNode.LeftChild;
+                replacementNode.LeftChild = this;
+                if (this.rightChild != null) { this.rightChild.ParentNode = this; }
+            }
+            //finally make our old child into our parentNode. (weird!)
+            this.parentNode = replacementNode; 
+        }
+
+        public AVLNode balance() {
+            //this function will balance the sides under this node
+            int leftWeight = 0, rightWeight = 0;
+            if (this.leftChild != null) {
+                //first balance the child
+                this.leftChild.balance();
+                //then pull its weight value in
+                leftWeight = this.leftChild.getDepth();
+            }
+            if (this.rightChild != null) {
+                this.rightChild.balance();
+                rightWeight = this.rightChild.getDepth();
+            }
+            //now we make our comparisons to determine if swaps need to be made
+            if (leftWeight > rightWeight + 1) {
+                //if one side significantly outweighs the other, make the swap
+                this.swap(this.leftChild);
+            } else if (rightWeight > leftWeight + 1) {
+                this.swap(this.rightChild);
+            }
+            //finally return the current parent node for reference
+            return this.parentNode; 
+        }
+
+        public void drawSelf(frmAVL currentForm) {
+            this.myLabel = new System.Windows.Forms.Label();
+            
+            currentForm.label1.AutoSize = true;
+            currentForm.label1.Location = new System.Drawing.Point(235, 9);
+            currentForm.label1.Name = "label1";
+            currentForm.label1.Size = new System.Drawing.Size(39, 13);
+            currentForm.label1.TabIndex = 3;
+            currentForm.label1.Text = "Depth:";
+        }
+    }
+}
---
+++ AVL Tool/AVL Tool/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace AVL_Tool
+{
+    partial class frmAVL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdAdd = new System.Windows.Forms.Button();
+            this.cmdBalance = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDepth = new System.Windows.Forms.Label();
+            this.lblList = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DrawingSurface = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // cmdAdd
+            // 
+            this.cmdAdd.Location = new System.Drawing.Point(13, -1);
+            this.cmdAdd.Name = "cmdAdd";
+            this.cmdAdd.Size = new System.Drawing.Size(80, 32);
+            this.cmdAdd.TabIndex = 0;
+            this.cmdAdd.Text = "Add Number";
+            this.cmdAdd.UseVisualStyleBackColor = true;
+            this.cmdAdd.Click += new System.EventHandler(this.cmdAdd_Click);
+            // 
+            // cmdBalance
+            // 
+            this.cmdBalance.Location = new System.Drawing.Point(12, 37);
+            this.cmdBalance.Name = "cmdBalance";
+            this.cmdBalance.Size = new System.Drawing.Size(80, 32);
+            this.cmdBalance.TabIndex = 1;
+            this.cmdBalance.Text = "Balance Tree";
+            this.cmdBalance.UseVisualStyleBackColor = true;
+            this.cmdBalance.Click += new System.EventHandler(this.cmdBalance_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(99, 6);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(69, 20);
+            this.txtNum.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Depth:";
+            // 
+            // lblDepth
+            // 
+            this.lblDepth.AutoSize = true;
+            this.lblDepth.Location = new System.Drawing.Point(269, 9);
+            this.lblDepth.Name = "lblDepth";
+            this.lblDepth.Size = new System.Drawing.Size(13, 13);
+            this.lblDepth.TabIndex = 4;
+            this.lblDepth.Text = "0";
+            // 
+            // lblList
+            // 
+            this.lblList.AutoSize = true;
+            this.lblList.Location = new System.Drawing.Point(347, 9);
+            this.lblList.Name = "lblList";
+            this.lblList.Size = new System.Drawing.Size(36, 13);
+            this.lblList.TabIndex = 6;
+            this.lblList.Text = "Empty";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(308, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Values:";
+            // 
+            // DrawingSurface
+            // 
+            this.DrawingSurface.Location = new System.Drawing.Point(101, 35);
+            this.DrawingSurface.Name = "DrawingSurface";
+            this.DrawingSurface.Size = new System.Drawing.Size(803, 440);
+            this.DrawingSurface.TabIndex = 7;
+            this.DrawingSurface.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawingSurface_Paint);
+            // 
+            // frmAVL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 483);
+            this.Controls.Add(this.DrawingSurface);
+            this.Controls.Add(this.lblList);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblDepth);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.cmdBalance);
+            this.Controls.Add(this.cmdAdd);
+            this.Name = "frmAVL";
+            this.Text = "AVL Tree Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdAdd;
+        private System.Windows.Forms.Button cmdBalance;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDepth;
+        private System.Windows.Forms.Label lblList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel DrawingSurface;
+    }
+}
+
