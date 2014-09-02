2594744374db2ac6a5d4384a13ce0706860ffe45
JacobCallahan <jacob.callahan05@gmail.com>
2014-09-01 23:53:18 -0400

    Making some changes to be in compliance with PEP8 standards.

--- Python/Thread Decorator.py
+++ Python/Thread Decorator.py
@@ -1,19 +1,20 @@
 import threading
 
 def easyThread(*args):
-	def wrapper(func):
-		def threadedFunction():
-			for arg in args:
-				print("Starting new thread with ", arg)
-				thread = threading.Thread(target=func, args=(arg,))
-				thread.start()
-		return threadedFunction
-	return wrapper
+    def wrapper(func):
+        def threadedFunction():
+            for arg in args:
+                print("Starting new thread with ", arg)
+                thread = threading.Thread(target=func, args=(arg,))
+                thread.start()
+        return threadedFunction
+    return wrapper
 
 @easyThread(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 def printNumber(myNum):
-	maxCount = myNum * 10
-	for i in range(maxCount):
-		print("Printing ", i, " from ", myNum, " thread")
+    """ Simply print out numbers until you reach the max number times 10"""
+    maxCount = myNum * 10
+    for i in range(maxCount):
+        print("Printing ", i, " from ", myNum, " thread")
 
 printNumber()
