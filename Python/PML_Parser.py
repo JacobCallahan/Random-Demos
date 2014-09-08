29b1da565b0d40ac59e1dc5862d277614939007f
JacobCallahan <jacob.callahan05@gmail.com>
2014-09-08 08:42:56 -0400

    cleaning up
    removing debugging prints as well as well as adding some more comments

--- Python/PML_Parser.py
+++ Python/PML_Parser.py
@@ -15,7 +15,8 @@ class PML_Parser():
 		self.fileStrings = []
 
 	def readFile(self, fileLocation):
-		inFile = open(fileLocation, "r")
+		""" We will read the file, split it into our sections, and pass it on """
+		inFile = open(str(fileLocation), "r")
 		fileString = inFile.read()
 		#use regex to split the file into html/pml chunks
 		regex = re.compile("<pml>(.*?)</pml>",re.IGNORECASE|re.MULTILINE|re.DOTALL)	 	
@@ -24,6 +25,7 @@ class PML_Parser():
 		inFile.close()
 
 	def writeFile(self, fileLocation):
+		""" Finally, we will write the file out """
 		outFile = open(fileLocation, "w")
 		for fileString in self.fileStrings:
 			for line in fileString:
@@ -33,31 +35,25 @@ class PML_Parser():
 
 	def compilePML(self):
 		""" This module will leave standard HTML intact and process PML"""
-		outer = 0
-		inner = 0
 		newStringGroup = []
 		newSubGroup = []
+		#loop through our main groups
 		for stringGroup in self.fileStrings:
+			#and out individual sub groups
+			inner = 0
 			for subGroup in stringGroup:
-				print ("Loop iteration ", inner)
 				if inner % 2 != 0:
 					#first we clean up the string
-					print ("cleaning string: ", subGroup)
 					subGroup = self.cleanPMLString(subGroup)
-					print ("resulting in: ", subGroup)
 					#then we execute the pml string to get our HTML
-					print ("executing pml")
 					subGroup = self.executePMLString(subGroup)
-					print ("resulting in: ", subGroup)
 				newSubGroup.append(subGroup)
 				inner = inner + 1
 			newStringGroup.append(newSubGroup)
-			outer = outer + 1
 
 		self.fileStrings = newStringGroup
 
 	def executePMLString(self, pmlString):
-		print ("Executing PML String: ", pmlString)
 		#we will temporarily hijack stdout 
 		buffer = StringIO()
 		stdoutSave = sys.stdout
@@ -73,19 +69,15 @@ class PML_Parser():
 		splitPMLString = pmlString.split("\n")
 		# remove extra lines from the top, if needed
 		while splitPMLString[0] == "":
-			print("Removing leading newlines from ", splitPMLString)
 			splitPMLString = splitPMLString[1:]
-			print("Resulting in ", splitPMLString)
 
 		# if we have extra indentation, remove them 
 		newPMLString = []
 		while splitPMLString[0][:4] == "    ":
-			print("Removing extra indentations")
 			for line in splitPMLString:
 				if len(line) > 3:
 					newPMLString.append(line[4:])
 			splitPMLString, newPMLString = newPMLString, []
-			print("Resulting in ", splitPMLString)
 
 		# return the new string by joining and adding line breaks
 		return "".join(line + "\n" for line in splitPMLString)
@@ -94,7 +86,7 @@ def main():
 	""" This main function prompts the user for input and acts on it """
 	myPMLParser = PML_Parser()
 	#prompt for the file location
-	fileLocation = input("Please enter the location of the pml file: ")
+	fileLocation = input("Please enter the location of the pml file (enclose in quotes): ")
 	#then we read the file into our container
 	myPMLParser.readFile(fileLocation)
 	#next, we compile the PML
